40c8a63fa3f0be38c8fe14939c1e7abb947ad905
Samuel Johnson <samrm310@gmail.com>
2021-01-21 22:07:47 -0300

    Adicionado menu hambúrguer responsivo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let botao_menu = document.getElementsByClassName('btn-menu');
+let botao_fechar_menu = document.getElementsByClassName('btn-close');
+let menu = document.getElementsByClassName('menus');
+
+botao_menu[0].addEventListener('click', clicou_menu);
+botao_fechar_menu[0].addEventListener('click', fechou_menu);
+
+function clicou_menu() {
+    menu[0].style.display = "block";
+};
+
+
+
+
+
+
+
